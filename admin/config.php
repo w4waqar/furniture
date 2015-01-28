eb00844c03d26279647469c85398f70f2716a5cd
Jaffar Raza <jaffaraza@gmail.com>
2015-01-28 12:17:06 +0400

    Furniture in open cart

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/furniture/admin/');
+define('HTTP_CATALOG', 'http://localhost/furniture/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/furniture/admin/');
+define('HTTPS_CATALOG', 'http://localhost/furniture/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\furniture/admin/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\furniture/system/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\furniture/admin/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\furniture/admin/view/template/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\furniture/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\furniture/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\furniture/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\furniture/system/download/');
+define('DIR_UPLOAD', 'D:\xampp\htdocs\furniture/system/upload/');
+define('DIR_LOGS', 'D:\xampp\htdocs\furniture/system/logs/');
+define('DIR_MODIFICATION', 'D:\xampp\htdocs\furniture/system/modification/');
+define('DIR_CATALOG', 'D:\xampp\htdocs\furniture/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'furniture');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/furniture/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/furniture/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\furniture/catalog/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\furniture/system/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\furniture/catalog/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\furniture/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\furniture/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\furniture/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\furniture/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\furniture/system/download/');
+define('DIR_UPLOAD', 'D:\xampp\htdocs\furniture/system/upload/');
+define('DIR_MODIFICATION', 'D:\xampp\htdocs\furniture/system/modification/');
+define('DIR_LOGS', 'D:\xampp\htdocs\furniture/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'furniture');
+define('DB_PREFIX', '');
